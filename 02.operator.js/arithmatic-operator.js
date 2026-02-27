05819566094b6d8a084cab077a2eb926ebd75d00
Rahul Deshmukh <rd0820121@gmail.com>
2026-02-27 11:52:54 +0530

    Add arithmetic operations in arithmatic-operator.js

---
+++ 02.operator.js/arithmatic-operator.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 5;
+
+console.log("Addition:", a + b);          //15
+console.log("Subtraction:", a - b);       //5
+console.log("Multiplication:", a * b);    //50
+console.log("Division:", a / b);          //2
+console.log("Remainder:", a % b);         //0
